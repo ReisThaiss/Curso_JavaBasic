c4bb59e5bd2659272d8503f12c38ea9c66b2d76a
Thais Reis <162192901+ReisThaiss@users.noreply.github.com>
2024-07-23 14:36:41 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,23 @@
+public class Cliente {
+    private String nome;
+
+    // Construtor
+    public Cliente() {
+        this.nome = nome;
+    }
+
+    Cliente() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Cliente() {
+        //TODO Auto-generated constructor stub
+    }
+
+    // Método getter para o nome
+    public String getNome() {
+        return nome;
+    }
+
+    // Outros métodos e atributos conforme necessário
+}
---
+++ ContaCorrente.java
@@ -0,0 +1,12 @@
+public class ContaCorrente extends Conta implements ImprimirExtrato {
+
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        super.imprimirInfosComuns();
+    }
+}
